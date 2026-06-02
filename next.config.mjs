1c0ebf3278a83a5e9b05d2831a8c6934357ae42e
kabanvershinin-film <kaban.vershinin@gmail.com>
2026-06-02 21:48:48 +0000

    Bump build id to v19 to force redeploy of face library

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
   experimental: {
     serverBodySizeLimit: '50mb',
   },
-  generateBuildId: async () => 'build-remove-runall-v18',
+  generateBuildId: async () => 'build-face-library-v19',
 };
 
 export default nextConfig;
